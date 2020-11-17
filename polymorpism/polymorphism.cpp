70819145726f5d9590e513d8cfeb1039782e0531
MAHESH-K <60285145+Mahesh-7@users.noreply.github.com>
2020-11-17 13:07:03 +0530

    Add files via upload

---
+++ polymorpism/polymorphism.cpp
@@ -0,0 +1,51 @@
+//ref:w3school
+
+#include <iostream>
+
+using namespace std;
+
+//polymorphism : it allow same function names with diff arg , we can access only function in individual class cant access other class functions (i.e) polymorphism
+
+// Base class
+class Animal {
+  public:
+    void animalSound1() {
+    cout << "The animal makes a sound \n" ;
+  }
+      void animalSound1(string msg) {
+    cout << "The animal makes a sound "<<msg<<endl ;
+  }
+};
+
+// Derived class
+class Pig : public Animal {
+  public:
+    void animalSound() {
+    cout << "The pig says: wee wee \n" ;
+   }
+};
+
+// Derived class
+class Dog : public Animal {
+  public:
+    void animalSound() {
+    cout << "The dog says: bow wow \n" ;
+  }
+};
+
+int main() {
+	
+  Animal myAnimal;
+  Pig myPig;
+  Dog myDog;
+
+  myAnimal.animalSound();
+  myAnimal.animalSound("different");
+  myPig.animalSound();
+  myDog.animalSound();
+  obj.animalSound("different");  //we cant Access base class function while same function name (i.e) polymorphism  
+  								//have to create individual obj for class to access function with same name
+  
+
+  return 0;
+}
