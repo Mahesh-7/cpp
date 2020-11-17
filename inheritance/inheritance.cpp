9280272e60e1d4b8f132c328f1be3f73b316ab6d
MAHESH-K <60285145+Mahesh-7@users.noreply.github.com>
2020-11-17 11:22:54 +0530

    Add files via upload

--- inheritance/inheritance.cpp
+++ inheritance/inheritance.cpp
@@ -4,24 +4,46 @@
 
 using namespace std;
 
-// Base class
-class Vehicle {
-  public:
-    string brand = "Ford";
-    void honk() {
-      cout << "Tuut, tuut! \n" ;
-    }
+
+class vehicle  //base class
+{
+	public:
+		
+		string brand="yamaha";
+		
+		void type()
+		{
+			cout<<"race bike"<<endl;
+		}
 };
 
-// Derived class
-class Car: public Vehicle {
-  public:
-    string model = "Mustang";
+class bike:public vehicle  //derived class
+{
+	public:
+		
+		string model="R15-V3";
+		
+		//cout<<"fast"<<endl;  //cant execute inside derived class directly without help of function
+		
+		void review()
+		{
+				cout<<"super"<<endl;  //can run inside derived class by using function
+		}
+	
+	
 };
 
-int main() {
-  Car myCar;
-  myCar.honk();
-  cout << myCar.brand + " " + myCar.model;
-  return 0;
+
+int main()
+{
+	bike obj; //obj created for derived class
+	
+	obj.type();
+	cout<<obj.brand<<endl;
+	cout<<obj.model<<endl;
+	obj.review();	
+	
+	//cout<<vehicle::brand;  //invalid
+	
+	return 0;
 }
