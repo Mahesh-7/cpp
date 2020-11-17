0550cba419189ab16aecf0eb15eb9994da47dc87
MAHESH-K <60285145+Mahesh-7@users.noreply.github.com>
2020-11-17 10:56:54 +0530

    Add files via upload

--- abstraction_encapsulation/class1.cpp
+++ abstraction_encapsulation/class1.cpp
@@ -1,64 +1,41 @@
-//ref:w3school
+//ref:geeks
 
 #include <iostream>
 
 using namespace std;
 
-class mobile  //user global class definition
-{
-	public:  //access specifier  ,,  by default private if we not used public keyword
+
+//abstraction : only shows essential info to users
+//encapsulation :it consists data variables and methods in single class unit and hidding data operations and show only results
+
+class Encapsulation 
+{ 
+	private: 
+	
+		// data hidden from outside world 
+		int x; 
 		
-	string brand; //attributes/variables
-	
-	string model;
-	
-	int price;
-	
-	mobile(string msg); //constructor declarationw with arg ,it doesnt need return-types
-	
-/*	
-	mobile() //local constructor with function declaration and definition
-	{
-		cout<< "welcome to mobile world"<<endl;
-	}
-	
-*/
-	
-	void mob_spec(string msg);//method/function declaration with arg
-	
-	
-/*	
-	void mob_spec(string msg)
-	{
-		cout<<msg<<endl<<brand<<" "<<model<<" "<<price<<endl;  //attribute access inside class
-	}
-*/
+	public: 
 
+		void set(int a) 
+		{ 
+			x =a; 
+		} 
+ 
+		int get()      //abstractions decide which want to show for user
+		{ 
+			return x; 
+		} 
+}; 
+
+int main() 
+{ 
+	Encapsulation obj; 
 	
-}mob("welcome to mobile world");   // Create an global object of MyClass with constructor argument
-
-
-void mobile::mob_spec(string msg)
-{
-	cout<<msg<<endl<<mob.brand<<" "<<mob.model<<" "<<mob.price<<endl;    //attribute access outside class
-}
-
-mobile::mobile(string msg)  //constructor no need to call from anywhere ,it call automatically when class obj created
-{
-	cout<<msg <<endl;
-}
-
-int main( )
-
-{
-//	mobile mob;   // Create an local object of MyClass
-	mob.brand="mi";
-	mob.model="7s";
-	mob.price=10000;
+	obj.set(5); 
 	
-	//cout<<mob.brand<<" "<<mob.model<<" "<<mob.price<<endl;   //attribute access outside class without method
+	cout<<obj.get(); 
 	
-	mob.mob_spec("latest mobiles");  // Call the method
-		
-	return 0;
-}
+	return 0; 
+} 
+
